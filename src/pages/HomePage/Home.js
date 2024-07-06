a0de227146b1f12afa8b56280d0ba3267829e430
RIYAN SAFFEEKH K M <138657295+riyan-hx@users.noreply.github.com>
2024-07-07 01:10:29 +0530

    Add files via upload

---
+++ src/pages/HomePage/Home.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+import '../HomePage/Home.css'
+
+import React from 'react'
+
+function Home() {
+  return (
+    <div className="Home">
+      
+    </div>
+  )
+}
+
+export default Home
