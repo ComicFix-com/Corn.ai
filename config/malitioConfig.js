b99242acb0256525ebf9a5f8177c8491932d2fec
Muhammed Adnan <147593005+muhammedadnanv@users.noreply.github.com>
2024-07-13 20:37:51 +0530

    Create malitioConfig.js

---
+++ config/malitioConfig.js
@@ -0,0 +1,13 @@
+const malitio = require('malitio');
+
+const mailTransporter = malitio.createTransport({
+    host: 'smtp.mailserver.com', // Replace with your SMTP server
+    port: 587, // Replace with your SMTP port
+    secure: false, // True for 465, false for other ports
+    auth: {
+        user: 'cornai@proton.me', 
+        pass: 'your-email-password', 
+    },
+});
+
+module.exports = mailTransporter;
