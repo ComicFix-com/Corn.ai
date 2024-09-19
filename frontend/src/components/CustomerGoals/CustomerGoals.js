8b4f29613a0e10b7b37c21027fb2fa37345945c4
jerryjohnsoffl <jerryjohnsoffl@gmail.com>
2024-09-19 23:22:03 +0530

    content added

--- frontend/src/components/CustomerGoals/CustomerGoals.js
+++ frontend/src/components/CustomerGoals/CustomerGoals.js
@@ -1,4 +1,6 @@
-import './CustomerGoals.css'
+import './CustomerGoals.css';
+import DownloadIcon from '../../img/download-icon.svg';
+import ShareIcon from '../../img/share-icon.svg'
 
 function CustomerGoals() {
     return(
@@ -7,7 +9,29 @@ function CustomerGoals() {
                 <h1>Recent interactions</h1>
                 <div className="container">
                     <div className="content">
-                        {/* <InnerContent /> */}
+                        <div className="inner-content">
+                            <div className="top flex">
+                                <div className="w-50">
+                                <h3>Conversational insights and customer interaction analytics</h3>
+                                <p>Evaluating customer engagement and conversational dynamics.</p>
+                                </div>
+                                <div className="w-50 flex top-r">
+                                    <div className="message">
+                                        
+                                    </div>
+                                    <div className="download-share flex">
+                                        <button>
+                                            <img src={DownloadIcon} alt="" />
+                                        </button>
+                                        <button>
+                                            <img src={ShareIcon} alt="" />
+                                        </button>
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="middle"></div>
+                            <div className="bottom"></div>
+                        </div>
                     </div>
                 </div>
             </div>
